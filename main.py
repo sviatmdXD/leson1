f3bde5efbe42a1b3c1586b2fab98b58bae6122be
NEGGER <kitsvat880@gmail.com>
2024-03-08 18:22:44 +0200

    first

---
+++ main.py
@@ -0,0 +1,32 @@
+class Student:
+    name="Alex"
+    def __int__(self):
+        self.name="Nick"
+        self.age=12
+        print(self.age)
+
+
+student1=Student()
+Student.__init__(self=student1)
+
+print(student1.name)
+
+
+class Student:
+    def __init__(self):
+        self.height = 170
+
+    height = 160
+
+    def printer(self):
+        print(self.height)
+
+
+nick = Student()
+nick.printer()
+
+class Student:
+
+
+
+
